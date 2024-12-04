95b670f81de1ba98cb52d6d75bf438c6181b5d75
molbe532 <molbe532@student.liu.se>
2024-12-04 13:21:03 +0100

    Adding some widgets and testing

---
+++ login_page/lib/presentation/widgets/password_feedback.dart
@@ -0,0 +1,9 @@
+/** File for checking wether or not the password
+ *  is weak (not ok), ok or strong!
+ *  Only logic and no UI in this one!!
+ * 
+ */
+
+class PasswordFeedback {}
+
+class CheckPassword {}
